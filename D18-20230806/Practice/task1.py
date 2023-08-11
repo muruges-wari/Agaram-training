39e2a4f93a19030b1518fcf3d50d9b17b8cdd2b1
muruges-wari <gayathrikarthi102@gmail.com>
2023-08-11 16:51:22 +0530

    HTML and CSS

--- D18-20230806/Practice/task1.py
+++ D18-20230806/Practice/task1.py
@@ -1,9 +1,15 @@
-number=int(input("Enter the number"))
-for i in range(number):
-    for j in range(number):
-        print("x",end="")
-    print("")
-    
+lst=["flower","flow","flight"]
+# position=0
+# for i in lst:
+#     if i//2:
+#         position+=1
+#         print(i)
 
-    
+same=""
+position=0
+for i in lst:
+    for j in i:
+        if j not in same:
+            position+=1
+            print(j)
 
---
+++ D18-20230806/Practice/task7.py
@@ -0,0 +1,5 @@
+number=int(input("Enter the number"))
+for i in range(number):
+    for j in range(number):
+        print("x",end="")
+    print("")
